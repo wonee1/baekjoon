5402ffec662cdc84fa7ac5b71690a566a1a5b61c
chaewon <99332779+gimchaewon@users.noreply.github.com>
2024-03-05 22:12:27 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -9,12 +9,12 @@ int main() {
 
     vector<int> scores(N);
 
-    // 세준이의 현재 성적 입력
+    // 성적 입력
     for (int i = 0; i < N; ++i) {
         cin >> scores[i];
     }
 
-    // 최댓값 찾기
+    // 최댓값 추출 
     int M = 0;
     for (int i = 0; i < N; ++i) {
         if (scores[i] > M) {
@@ -24,15 +24,15 @@ int main() {
 
     double sum = 0;
 
-    // 새로운 점수 계산 및 합 구하기
+    // 점수 보정과 점수 합 계산 
     for (int i = 0; i < N; ++i) {
         sum += (double)scores[i] / M * 100;
     }
 
-    // 평균 출력
+    
     cout << fixed;  // 소수점 아래 자리수 고정
     cout.precision(2);  // 소수점 아래 2자리까지 출력
-    cout << sum / N << endl;
+    cout << sum / N << endl; // 평균 출력
 
     return 0;
 }
