5cb74b8e093d4d506e68a5ca01236a271d057fba
chaewon <99332779+gimchaewon@users.noreply.github.com>
2024-04-11 14:54:59 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String id=in.next();//id 입력 
+		System.out.printf("%s??!\n",id);
+		
+
+	}
+}
