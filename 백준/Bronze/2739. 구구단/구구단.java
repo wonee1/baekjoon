4a0400ef2d7ff1173f974048210857ecdca6059b
chaewon <99332779+gimchaewon@users.noreply.github.com>
2024-04-11 18:23:08 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16200 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
-public class Main
-{
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		int dan=in.nextInt();
-        int res=0;
-        
-        
-        for(int i=1;i<10;i++){
-            res=dan*i;
-            System.out.printf("%d * %d = %d\n", dan, i, res);
-            
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int dan = Integer.parseInt(reader.readLine());
+
+            for (int i = 1; i <= 9; i++) {
+                int res = dan * i;
+                writer.write(dan + " * " + i + " = " + res + "\n");
+            }
+
+            writer.flush();
+            writer.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
-       
- 	}
+    }
 }
