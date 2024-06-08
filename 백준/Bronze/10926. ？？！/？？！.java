8fd0dabbf068dc404845b0ebf538ad522fb1f3eb
chaewon <99332779+gimchaewon@users.noreply.github.com>
2024-06-08 21:37:20 +0900

    [Bronze V] Title: ??!, Time: 136 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main
-{
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		String id=in.next();//id 입력 
-		System.out.printf("%s??!\n",id);
-		
-
-	}
+public class Main{
+    
+    public static void main(String args[])throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id= br.readLine();
+        System.out.printf("%s??!\n",id);
+    }
 }
