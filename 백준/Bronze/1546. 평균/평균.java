b463fbb057884389e3b88b4addb24eaee459c3ff
chaewon <99332779+wonee1@users.noreply.github.com>
2024-08-14 18:29:11 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());// 개수 입력 받기 
+        String[] input = br.readLine().split(" ");// 점수를 공백으로 분리 
+        int[] A = new int[N];
+        double max=0;
+        double sum=0;
+        
+        for(int i=0;i<N;i++){
+           A[i] = Integer.parseInt(input[i]); 
+        }
+        
+        
+        for(int i=0;i<N;i++){
+            if(max<A[i])max=A[i];
+            sum+=A[i];
+        }
+        
+        System.out.print(sum*100/max/N);
+      
+    }
+}
