98d311cfdd37f98808c4f4ea720ccf2f112215ff
chaewon <99332779+wonee1@users.noreply.github.com>
2025-09-25 11:53:45 +0900

    [Bronze V] Title: 기찍 N, Time: 828 ms, Memory: 32360 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt(); 
+        
+        
+        for(int i=n; i>0;i--){
+            System.out.println(i); 
+        }
+    }
+}
