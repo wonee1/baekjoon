c03af1973d285eb23f5947b4d63fdd41b5330319
chaewon <99332779+wonee1@users.noreply.github.com>
2026-03-22 16:38:31 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String n_str) {
+        
+      String result = Integer.toString(Integer.parseInt(n_str));
+       
+      return result; 
+        
+        
+    }
+}
