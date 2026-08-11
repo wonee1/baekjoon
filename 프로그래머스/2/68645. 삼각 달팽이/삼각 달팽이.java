50d3d6061a790b498de1581b4c51d918621b41bf
chaewon <99332779+wonee1@users.noreply.github.com>
2026-08-11 12:48:58 +0900

    [level 2] Title: 삼각 달팽이, Time: 12.34 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,46 +1,34 @@
 class Solution {
+    
+    private static final int[] dx = {0,1,-1};
+    private static final int[] dy = {1,0,-1};  // 변화량 사용 아래->오른쪽->왼쪽 위 순서 
+    
+    
     public int[] solution(int n) {
         
         
         int[][] triangle = new int[n][n];
         int v = 1; //채워넣을 숫자 
-        int x = 0; 
+        int x = 0;                                              
         int y = 0; 
         //숫자를 채운 현재 위치를 0,0으로 설정 
-        
+        int d = 0; 
+                
        while(true){  
+           triangle[y][x] = v++; 
+           int nx = x + dx[d]; 
+           int ny = y + dy[d]; 
+           if(nx == n || ny ==n || nx==-1 || ny==-1 ||triangle[ny][nx]!=0){
+               d = (d+1)%3; 
+               nx = x+dx[d];
+               ny = y+dy[d]; 
+               if(nx == n || ny ==n || nx==-1 || ny==-1 ||triangle[ny][nx]!=0) break; 
            
-        //아래로 이동
-        while(true){
-            triangle[y][x] = v++; 
-            if (y+1 == n || triangle[y+1][x]!=0) break;
-            y+=1; 
+            }
+               x = nx; 
+               y = ny; 
         }
-        if(x+1 == n || triangle[y][x+1]!=0) break; // 오른쪽 칸이 막혀있는지 확인 
-        x+=1; 
-        
-        
-        //오른쪽으로 이동
-        while(true){
-            triangle[y][x] = v++;
-            if(x+1 == n || triangle[y][x+1]!=0) break; 
-            x+=1; 
-        }
-        if(triangle[y-1][x-1]!=0)break; //대각선 칸이 막혀있는지 확인 
-        x-=1;
-        y-=1;
-        
-        //대각선으로 이동하면서 숫자 채우기 
-        while(true){
-            triangle[y][x] = v++;
-            if(triangle[y-1][x-1]!=0) break; 
-            x -=1; 
-            y -=1; 
-        }
-        if(y+1 == n || triangle[y+1][x] != 0) break; //아래로 한칸 내리기 전에 확인 
-        y+=1; 
-        
-       }
+
         
         int[] result = new int[v-1]; 
         int index = 0;
